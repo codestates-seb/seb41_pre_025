e4ec374a0fda1cfff7654771c2abcb42094798d6
orioncsy <orioncsy@gmail.com>
2022-12-21 22:30:09 +0900

    Fix (#18)
    * feat : add question CRUD and create question API docs
    
    * fix : use interface for DI and beanUtils
    
    * fix : handle MockQuestionRepository class
    
    * fix : remove classes related with mock
    
    * feat : fe first setting (#15)
    
    * docs : BE 기능 템플릿
    
    * docs : bug issue template
    
    버그 관련 이슈 템플릿 입니다.
    
    * Update issue templates
    
    * feat: fe first setting
    
    Co-authored-by: yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
    Co-authored-by: RiOAOn <rian0422@naver.com>
    
    Co-authored-by: yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
    Co-authored-by: Lee Hyun Ji <107021273+Hyunji0911@users.noreply.github.com>
    Co-authored-by: RiOAOn <rian0422@naver.com>

--- server/src/main/java/com/overflow/stack/server/domain/question/repository/QuestionRepository.java
+++ server/src/main/java/com/overflow/stack/server/domain/question/repository/QuestionRepository.java
@@ -1,4 +1,9 @@
 package com.overflow.stack.server.domain.question.repository;
 
-public interface QuestionRepository {
+import com.overflow.stack.server.domain.question.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
