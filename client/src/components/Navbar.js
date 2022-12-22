b4243b67137ff19d6e9e5ff286d694d665f6e91e
wihyun <110884421+wihyun@users.noreply.github.com>
2022-12-22 13:55:39 +0900

    style: footer add css & link (#24)
    fix : prettier로 인한 충돌 수정

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -48,6 +48,7 @@ function Navbar() {
 			</NavbarContainer>
 		</>
 	);
+
 }
 const NavbarContainer = styled.div`
 	position: sticky;
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -2,34 +2,28 @@ import React from "react";
 import styled from "styled-components";
 
 function Sidebar() {
-  return <>
-    <SidebarContainer>
-      <ul>
-        <li>Home</li>
-        <li>PUBLIC</li>
-      <ul> 
-        <li>
-          Questions
-        </li>
-        <li>
-          Tags
-        </li>
-        <li>
-          Users
-        </li>
-        <li>
-          Companies
-        </li>
-      </ul>
-      </ul>
-    </SidebarContainer>
-  </>
+  return (
+    <>
+      <SidebarContainer>
+        <ul>
+          <li>Home</li>
+          <li>PUBLIC</li>
+          <ul>
+            <li>Questions</li>
+            <li>Tags</li>
+            <li>Users</li>
+            <li>Companies</li>
+          </ul>
+        </ul>
+      </SidebarContainer>
+    </>
+  );
 }
 
 const SidebarContainer = styled.div`
-    width: 167px;
-    height: 100vh;
-    border-right: 1px solid lightgray;
-  `;
+  width: 167px;
+  height: 100vh;
+  border-right: 1px solid lightgray;
+`;
 
 export default Sidebar;
