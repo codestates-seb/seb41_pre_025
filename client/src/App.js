727282b988a27318a3e388d8e4675e087034f045
Hyunji0911 <hyunji.lee0911@gmail.com>
2022-12-22 15:07:28 +0900

    feature: login first setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Template from './components/Template';
-import Navbar from './components/Navbar';
+import Template from "./components/Template";
+import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
-import Footer from './components/Footer';
-import Ad from './components/Ad';
-import Section from './components/Section';
-import TopQuestions from './components/TopQuestions';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import QuestionsList from './components/QuestionsList';
-import AskQuestion from './components/AskQuestion';
-import Mypage from './components/Mypage';
+import Footer from "./components/Footer";
+import Ad from "./components/Ad";
+import Section from "./components/Section";
+import TopQuestions from "./components/TopQuestions";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import QuestionsList from "./components/QuestionsList";
+import AskQuestion from "./components/AskQuestion";
+import Mypage from "./components/Mypage";
 
 const GlobalStyle = createGlobalStyle`
 * {
@@ -30,27 +30,39 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 function App() {
-  return (
-    <BrowserRouter>
-        <GlobalStyle />
-        <Navbar/>
-        <Template>
-        <Sidebar/>
-        <Section>
-          <Routes>
-            <Route path="/" element={<TopQuestions />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/signup" element={<SignUp />}></Route>
-            <Route path="/questions" element={<QuestionsList />}></Route>
-            <Route path="/askquestions" element={<AskQuestion />}></Route>
-            <Route path="/mypage" element={<Mypage />}></Route>
-          </Routes>
-        </Section>
-          <Ad/>
-        </Template>
-        <Footer/>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<GlobalStyle />
+			<Navbar />
+			<Template>
+				<Sidebar />
+				<Section>
+					<Routes>
+						<Route
+							path="/"
+							element={<TopQuestions />}></Route>
+						<Route
+							path="/login"
+							element={<Login />}></Route>
+						<Route
+							path="/signup"
+							element={<SignUp />}></Route>
+						<Route
+							path="/questions"
+							element={<QuestionsList />}></Route>
+						<Route
+							path="/askquestions"
+							element={<AskQuestion />}></Route>
+						<Route
+							path="/mypage"
+							element={<Mypage />}></Route>
+					</Routes>
+				</Section>
+				<Ad />
+			</Template>
+			<Footer />
+		</BrowserRouter>
+	);
 }
 
 export default App;
