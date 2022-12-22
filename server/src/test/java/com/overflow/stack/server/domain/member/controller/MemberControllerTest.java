e93494db2a5eac06b455d97bba9ff970067daa27
yoo hyeong chan <gudcks305@gmail.com>
2022-12-23 00:22:48 +0900

    test: member get Info RestDocs

--- server/src/test/java/com/overflow/stack/server/domain/member/controller/MemberControllerTest.java
+++ server/src/test/java/com/overflow/stack/server/domain/member/controller/MemberControllerTest.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.overflow.stack.server.auth.token.AuthTokenProvider;
 import com.overflow.stack.server.common.abstractControllerTest;
 import com.overflow.stack.server.domain.member.dto.MemberDto;
+import com.overflow.stack.server.domain.member.entity.Member;
+import com.overflow.stack.server.domain.member.factory.MemberFactory;
 import com.overflow.stack.server.domain.member.mapper.MemberMapper;
 import com.overflow.stack.server.domain.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.restdocs.payload.RequestFieldsSnippet;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -22,7 +25,11 @@ import org.springframework.test.web.servlet.ResultActions;
 import static com.overflow.stack.server.domain.member.factory.MemberFactory.createMemberPostDto;
 import static com.overflow.stack.server.util.ApiDocumentUtils.getRequestPreProcessor;
 import static com.overflow.stack.server.util.ApiDocumentUtils.getResponsePreProcessor;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -99,6 +106,55 @@ class MemberControllerTest extends abstractControllerTest {
                 )
         ));
     }
+    @Test
+    @DisplayName("회원 정보 GET")
+    @WithMockUser(username = "test@gmail.com", roles = "USER")
+    void getMember() throws Exception {
+        // given
+        MemberDto.Response response = MemberFactory.createMemberResponseDto();
+        given(memberService.findMember(anyString())).willReturn(new Member());
+        given(mapper.memberToResponseMemberDto(any(Member.class))).willReturn(response);
+        // when
+
+        ResultActions resultActions = mockMvc.perform(get(BASE_URL)
+                        .contentType("application/json")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("data.email").value(response.getEmail()))
+                .andExpect(jsonPath("data.fullName").value(response.getFullName()))
+                .andExpect(jsonPath("data.displayName").value(response.getDisplayName()))
+                .andExpect(jsonPath("data.aboutMe").value(response.getAboutMe()))
+                .andExpect(jsonPath("data.aboutMeTitle").value(response.getAboutMeTitle()))
+                .andExpect(jsonPath("data.twitterLink").value(response.getTwitterLink()))
+                .andExpect(jsonPath("data.githubLink").value(response.getGithubLink()))
+                .andExpect(jsonPath("data.websiteLink").value(response.getWebsiteLink()))
+                .andExpect(jsonPath("data.location").value(response.getLocation()))
+                .andExpect(jsonPath("data.imgUrl").value(response.getImgUrl()));
+        // then
+        resultActions.andDo(document("member-get",
+                getRequestPreProcessor(),
+                getResponsePreProcessor(),
+                getResponseFieldsSnippet()
+        ));
+
+
+    }
+
+    private static ResponseFieldsSnippet getResponseFieldsSnippet() {
+        return responseFields(
+                fieldWithPath("data").type(JsonFieldType.OBJECT).description("회원 정보"),
+                fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일"),
+                fieldWithPath("data.fullName").type(JsonFieldType.STRING).description("이름"),
+                fieldWithPath("data.displayName").type(JsonFieldType.STRING).description("닉네임"),
+                fieldWithPath("data.aboutMe").type(JsonFieldType.STRING).description("자기소개"),
+                fieldWithPath("data.aboutMeTitle").type(JsonFieldType.STRING).description("자기소개 제목"),
+                fieldWithPath("data.twitterLink").type(JsonFieldType.STRING).description("트위터 링크"),
+                fieldWithPath("data.githubLink").type(JsonFieldType.STRING).description("깃허브 링크"),
+                fieldWithPath("data.websiteLink").type(JsonFieldType.STRING).description("웹사이트 링크"),
+                fieldWithPath("data.location").type(JsonFieldType.STRING).description("지역"),
+                fieldWithPath("data.imgUrl").type(JsonFieldType.STRING).description("이미지 링크")
+        );
+    }
 
     private static RequestFieldsSnippet getRequestFieldsSnippet() {
         return requestFields(
@@ -115,6 +171,7 @@ class MemberControllerTest extends abstractControllerTest {
                 fieldWithPath("imgUrl").type(JsonFieldType.STRING).description("이미지 링크 /  nullable")
         );
     }
+
 
 
 }
