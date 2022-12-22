66aac8f9b34745fc0a1f537d95e91444ae37d7d9
yoo hyeong chan <gudcks305@gmail.com>
2022-12-23 00:24:21 +0900

    chore : email updatable 추가

--- server/src/main/java/com/overflow/stack/server/domain/member/entity/Member.java
+++ server/src/main/java/com/overflow/stack/server/domain/member/entity/Member.java
@@ -20,7 +20,7 @@ public class Member extends Auditable {
     @Column(name = "member_id")
     // only for mapping
     private Long memberId;
-    @Column(nullable = false, unique = true, length = 20)
+    @Column(nullable = false,updatable = false, unique = true, length = 20)
     private String email;
     @Column(nullable = false, length = 100)
     private String password;
