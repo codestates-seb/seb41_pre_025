27da54b02beaebf01354a10a80ea4ee47b7500ed
Hyunji0911 <hyunji.lee0911@gmail.com>
2022-12-23 18:02:27 +0900

    style : AskQuestion modify css

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
-import { createGlobalStyle } from "styled-components";
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { createGlobalStyle, css } from "styled-components";
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	useLocation,
+} from "react-router-dom";
 import Template from "./components/Template";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
@@ -15,48 +20,75 @@ import AskQuestion from "./components/AskQuestion";
 import Mypage from "./components/Mypage";
 import Pre from "./components/Pre";
 
+function App() {
+	const location = useLocation();
+	const pathname = location.pathname;
+	const sidebar = ["/login", "/signup", "/askquestions"];
+	const ad = [...sidebar, "/mypage"];
+	const footer = ["/login", "/signup"];
+	return (
+		<>
+			<GlobalStyle pathname={pathname} />
+			<Navbar />
+			<Template>
+				{sidebar.indexOf(pathname) === -1 && <Sidebar />}
+				<Section>
+					<Routes>
+						<Route
+							path="/"
+							element={<TopQuestions />}></Route>
+						<Route
+							path="/login"
+							element={<Login />}></Route>
+						<Route
+							path="/signup"
+							element={<SignUp />}></Route>
+						<Route
+							path="/QuestiosList"
+							element={<QuestionsList />}></Route>
+						<Route
+							path="/askquestions"
+							element={<AskQuestion />}></Route>
+						<Route
+							path="/mypage"
+							element={<Mypage />}></Route>
+						<Route
+							path="/tags"
+							element={<Pre />}></Route>
+						<Route
+							path="/users"
+							element={<Pre />}></Route>
+						<Route
+							path="/companies"
+							element={<Pre />}></Route>
+					</Routes>
+				</Section>
+				{ad.indexOf(pathname) === -1 && <Ad />}
+			</Template>
+			{footer.indexOf(pathname) === -1 && <Footer />}
+		</>
+	);
+}
+
 const GlobalStyle = createGlobalStyle`
 * {
   box-sizing: border-box;
 
 }
 
+body{
+      background-color:white;
+      ${(props) =>
+				["/askquestions"].indexOf(props.pathname) !== -1 &&
+				css`
+					background-color: #f1f2f3;
+				`}
+      }
+
   ol, ul, li {
 	list-style: none;
   }
   
 `;
-
-function App() {
-  const location = useLocation();
-  const pathname = location.pathname;
-  const sidebar = ["/login", "/signup"];
-  const ad = [...sidebar, "/mypage"];
-  const footer = [...sidebar];
-  return (
-    <>
-      <GlobalStyle />
-      <Navbar />
-      <Template>
-        {sidebar.indexOf(pathname) === -1 && <Sidebar />}
-        <Section>
-          <Routes>
-            <Route path="/" element={<TopQuestions />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/signup" element={<SignUp />}></Route>
-            <Route path="/QuestiosList" element={<QuestionsList />}></Route>
-            <Route path="/askquestions" element={<AskQuestion />}></Route>
-            <Route path="/mypage" element={<Mypage />}></Route>
-            <Route path="/tags" element={<Pre />}></Route>
-            <Route path="/users" element={<Pre />}></Route>
-            <Route path="/companies" element={<Pre />}></Route>
-          </Routes>
-        </Section>
-        {ad.indexOf(pathname) === -1 && <Ad />}
-      </Template>
-      {footer.indexOf(pathname) === -1 && <Footer />}
-    </>
-  );
-}
 
 export default App;
