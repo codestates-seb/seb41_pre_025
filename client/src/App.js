c9a13539fc4d85ff39ed6e957656260843821507
Lee Hyun Ji <107021273+Hyunji0911@users.noreply.github.com>
2022-12-26 14:38:31 +0900

    Feature/fe/ui014 (#70)
    * add pages file
    
    * style: modify css
    
    * feature: create pre page

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import SignUp from "./pages/SignUp";
 import QuestionsList from "./pages/QuestionsList";
 import AskQuestion from "./pages/AskQuestion";
 import Mypage from "./pages/Mypage";
-import Pre from "./components/Pre";
+import Pre from "./pages/Pre";
 import Question from "./pages/Question";
 
 const GlobalStyle = createGlobalStyle`
@@ -28,38 +28,50 @@ const GlobalStyle = createGlobalStyle`
       body{
       background-color:white;
       ${(props) =>
-        props.pathname === "/askquestions"
-          ? css`
-              background-color: #f7f8f8;
-            `
-          : ["/login", "/signup"].indexOf(props.pathname) !== -1 &&
-            css`
-              background-color: #f1f2f3;
-            `}
+				props.pathname === "/askquestions"
+					? css`
+							background-color: #f7f8f8;
+					  `
+					: ["/login", "/signup"].indexOf(props.pathname) !== -1 &&
+					  css`
+							background-color: #f1f2f3;
+					  `}
       }
 `;
 
 function App() {
-  const location = useLocation();
-  const pathname = location.pathname;
-  const sidebar = ["/login", "/signup", "/askquestions"];
-  const ad = [...sidebar, "/mypage"];
-  const footer = ["/login", "/signup"];
-  return (
-    <>
-      <GlobalStyle pathname={pathname} />
-      <Navbar />
-      <Template>
-        {sidebar.indexOf(pathname) === -1 && <Sidebar />}
-        <Section>
-          <Routes>
-            <Route path="/" element={<TopQuestions />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/signup" element={<SignUp />}></Route>
-            <Route path="/QuestiosList" element={<QuestionsList />}></Route>
-            <Route path="/askquestions" element={<AskQuestion />}></Route>
-            <Route path="/mypage" element={<Mypage />}></Route>
-            {/* 임시로 질문상세페이지를 tags에서 볼 수 있도록 설정
+	const location = useLocation();
+	const pathname = location.pathname;
+	const sidebar = ["/login", "/signup", "/askquestions"];
+	const ad = [...sidebar, "/mypage", "/users", "/companies"];
+	const footer = ["/login", "/signup"];
+	return (
+		<>
+			<GlobalStyle pathname={pathname} />
+			<Navbar />
+			<Template>
+				{sidebar.indexOf(pathname) === -1 && <Sidebar />}
+				<Section>
+					<Routes>
+						<Route
+							path="/"
+							element={<TopQuestions />}></Route>
+						<Route
+							path="/login"
+							element={<Login />}></Route>
+						<Route
+							path="/signup"
+							element={<SignUp />}></Route>
+						<Route
+							path="/QuestiosList"
+							element={<QuestionsList />}></Route>
+						<Route
+							path="/askquestions"
+							element={<AskQuestion />}></Route>
+						<Route
+							path="/mypage"
+							element={<Mypage />}></Route>
+						{/* 임시로 질문상세페이지를 tags에서 볼 수 있도록 설정
             데이터 받아온 후에 다시 변경해야 함 */}
             <Route path="/tags" element={<Question />}></Route>
             <Route path="/users" element={<Pre />}></Route>
