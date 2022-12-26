eb316c2a8e88f61986b232f801730eb5a9681871
Hyunji0911 <hyunji.lee0911@gmail.com>
2022-12-26 10:20:53 +0900

    feat: prettier & eslint settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { createGlobalStyle, css } from "styled-components";
 import {
@@ -20,26 +19,6 @@ import QuestionsList from "./components/QuestionsList";
 import AskQuestion from "./components/AskQuestion";
 import Mypage from "./components/Mypage";
 import Pre from "./components/Pre";
-=======
-
-import React from 'react';
-import { createGlobalStyle } from 'styled-components';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Template from './components/Template';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import Footer from './components/Footer';
-import Ad from './components/Ad';
-import Section from './components/Section';
-import TopQuestions from './components/TopQuestions';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import QuestionsList from './components/QuestionsList';
-import AskQuestion from './components/AskQuestion';
-import Mypage from './components/Mypage';
-import Pre from './components/Pre';
-import Question from './components/Question';
->>>>>>> a3c98fdb9c97d20cfe84cfca38cd99d4db6a3b2b
 
 function App() {
 	const location = useLocation();
@@ -112,41 +91,4 @@ body{
   
 `;
 
-<<<<<<< HEAD
-=======
-function App() {
-  const location = useLocation();
-  const pathname = location.pathname;
-  const sidebar = ["/login", "/signup"];
-  const ad = [...sidebar, "/mypage"];
-  const footer = [...sidebar];
-  return (
-    <>
-      <GlobalStyle />
-      <Navbar />
-      <Template>
-        {sidebar.indexOf(pathname) === -1 && <Sidebar />}
-        <Section>
-          <Routes>
-            <Route path="/" element={<TopQuestions />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/signup" element={<SignUp />}></Route>
-            <Route path="/QuestiosList" element={<QuestionsList />}></Route>
-            <Route path="/askquestions" element={<AskQuestion />}></Route>
-            <Route path="/mypage" element={<Mypage />}></Route>
-            {/* 임시로 질문상세페이지를 tags에서 볼 수 있도록 설정
-				데이터 받아온 후에 다시 변경해야 함 */}
-            <Route path="/tags" element={<Question />}></Route>
-            <Route path="/users" element={<Pre />}></Route>
-            <Route path="/companies" element={<Pre />}></Route>
-          </Routes>
-        </Section>
-        {ad.indexOf(pathname) === -1 && <Ad />}
-      </Template>
-      {footer.indexOf(pathname) === -1 && <Footer />}
-    </>
-  );
-}
-
->>>>>>> a3c98fdb9c97d20cfe84cfca38cd99d4db6a3b2b
 export default App;
