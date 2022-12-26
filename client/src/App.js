620b0d3af42658326aae9edc7d3bac686cd5dc9b
wihyun <110884421+wihyun@users.noreply.github.com>
2022-12-26 14:21:40 +0900

    style: modify css (#69)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
 import { createGlobalStyle, css } from "styled-components";
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Template from "./components/Template";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
@@ -22,10 +17,10 @@ import Pre from "./components/Pre";
 import Question from "./pages/Question";
 
 const GlobalStyle = createGlobalStyle`
-* {
-  box-sizing: border-box;
+  * {
+    box-sizing: border-box;
 
-}
+  }
 
   ol, ul, li {
    list-style: none;
@@ -33,67 +28,49 @@ const GlobalStyle = createGlobalStyle`
       body{
       background-color:white;
       ${(props) =>
-				props.pathname === "/askquestions"
-					? css`
-							background-color: #f7f8f8;
-					  `
-					: ["/login", "/signup"].indexOf(props.pathname) !== -1 &&
-					  css`
-							background-color: #f1f2f3;
-					  `}
+        props.pathname === "/askquestions"
+          ? css`
+              background-color: #f7f8f8;
+            `
+          : ["/login", "/signup"].indexOf(props.pathname) !== -1 &&
+            css`
+              background-color: #f1f2f3;
+            `}
       }
 `;
 
 function App() {
-	const location = useLocation();
-	const pathname = location.pathname;
-	const sidebar = ["/login", "/signup", "/askquestions"];
-	const ad = [...sidebar, "/mypage"];
-	const footer = ["/login", "/signup"];
-	return (
-		<>
-			<GlobalStyle pathname={pathname} />
-			<Navbar />
-			<Template>
-				{sidebar.indexOf(pathname) === -1 && <Sidebar />}
-				<Section>
-					<Routes>
-						<Route
-							path="/"
-							element={<TopQuestions />}></Route>
-						<Route
-							path="/login"
-							element={<Login />}></Route>
-						<Route
-							path="/signup"
-							element={<SignUp />}></Route>
-						<Route
-							path="/QuestiosList"
-							element={<QuestionsList />}></Route>
-						<Route
-							path="/askquestions"
-							element={<AskQuestion />}></Route>
-						<Route
-							path="/mypage"
-							element={<Mypage />}></Route>
-						{/* 임시로 질문상세페이지를 tags에서 볼 수 있도록 설정
+  const location = useLocation();
+  const pathname = location.pathname;
+  const sidebar = ["/login", "/signup", "/askquestions"];
+  const ad = [...sidebar, "/mypage"];
+  const footer = ["/login", "/signup"];
+  return (
+    <>
+      <GlobalStyle pathname={pathname} />
+      <Navbar />
+      <Template>
+        {sidebar.indexOf(pathname) === -1 && <Sidebar />}
+        <Section>
+          <Routes>
+            <Route path="/" element={<TopQuestions />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+            <Route path="/QuestiosList" element={<QuestionsList />}></Route>
+            <Route path="/askquestions" element={<AskQuestion />}></Route>
+            <Route path="/mypage" element={<Mypage />}></Route>
+            {/* 임시로 질문상세페이지를 tags에서 볼 수 있도록 설정
             데이터 받아온 후에 다시 변경해야 함 */}
-						<Route
-							path="/tags"
-							element={<Question />}></Route>
-						<Route
-							path="/users"
-							element={<Pre />}></Route>
-						<Route
-							path="/companies"
-							element={<Pre />}></Route>
-					</Routes>
-				</Section>
-				{ad.indexOf(pathname) === -1 && <Ad />}
-			</Template>
-			{footer.indexOf(pathname) === -1 && <Footer />}
-		</>
-	);
+            <Route path="/tags" element={<Question />}></Route>
+            <Route path="/users" element={<Pre />}></Route>
+            <Route path="/companies" element={<Pre />}></Route>
+          </Routes>
+        </Section>
+        {ad.indexOf(pathname) === -1 && <Ad />}
+      </Template>
+      {footer.indexOf(pathname) === -1 && <Footer />}
+    </>
+  );
 }
 
 export default App;
