5b52a772b43fbfa5cfc9b2008ecc63091eccb60a
RiOAOn <111356791+RiOAOn@users.noreply.github.com>
2022-12-26 17:19:59 +0900

    style: question page modify css (#75)

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { createGlobalStyle, css } from "styled-components";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Template from "./components/Template";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-import Footer from "./components/Footer";
-import Ad from "./components/Ad";
-import Section from "./components/Section";
-import TopQuestions from "./pages/TopQuestions";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import QuestionsList from "./pages/QuestionsList";
-import AskQuestion from "./pages/AskQuestion";
-import Mypage from "./pages/Mypage";
-import Pre from "./pages/Pre";
-import Question from "./pages/Question";
+import React from 'react';
+import { createGlobalStyle, css } from 'styled-components';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Template from './components/Template';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+import Ad from './components/Ad';
+import Section from './components/Section';
+import TopQuestions from './pages/TopQuestions';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import QuestionsList from './pages/QuestionsList';
+import AskQuestion from './pages/AskQuestion';
+import Mypage from './pages/Mypage';
+import Pre from './pages/Pre';
+import Question from './pages/Question';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -28,50 +28,38 @@ const GlobalStyle = createGlobalStyle`
       body{
       background-color:white;
       ${(props) =>
-				props.pathname === "/askquestions"
-					? css`
-							background-color: #f7f8f8;
-					  `
-					: ["/login", "/signup"].indexOf(props.pathname) !== -1 &&
-					  css`
-							background-color: #f1f2f3;
-					  `}
+        props.pathname === '/askquestions'
+          ? css`
+              background-color: #f7f8f8;
+            `
+          : ['/login', '/signup'].indexOf(props.pathname) !== -1 &&
+            css`
+              background-color: #f1f2f3;
+            `}
       }
 `;
 
 function App() {
-	const location = useLocation();
-	const pathname = location.pathname;
-	const sidebar = ["/login", "/signup", "/askquestions"];
-	const ad = [...sidebar, "/mypage", "/users", "/companies"];
-	const footer = ["/login", "/signup"];
-	return (
-		<>
-			<GlobalStyle pathname={pathname} />
-			<Navbar />
-			<Template>
-				{sidebar.indexOf(pathname) === -1 && <Sidebar />}
-				<Section>
-					<Routes>
-						<Route
-							path="/"
-							element={<TopQuestions />}></Route>
-						<Route
-							path="/login"
-							element={<Login />}></Route>
-						<Route
-							path="/signup"
-							element={<SignUp />}></Route>
-						<Route
-							path="/QuestiosList"
-							element={<QuestionsList />}></Route>
-						<Route
-							path="/askquestions"
-							element={<AskQuestion />}></Route>
-						<Route
-							path="/mypage"
-							element={<Mypage />}></Route>
-						{/* 임시로 질문상세페이지를 tags에서 볼 수 있도록 설정
+  const location = useLocation();
+  const pathname = location.pathname;
+  const sidebar = ['/login', '/signup', '/askquestions'];
+  const ad = [...sidebar, '/mypage', '/users', '/companies'];
+  const footer = ['/login', '/signup'];
+  return (
+    <>
+      <GlobalStyle pathname={pathname} />
+      <Navbar />
+      <Template>
+        {sidebar.indexOf(pathname) === -1 && <Sidebar />}
+        <Section>
+          <Routes>
+            <Route path="/" element={<TopQuestions />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+            <Route path="/questionslist" element={<QuestionsList />}></Route>
+            <Route path="/askquestions" element={<AskQuestion />}></Route>
+            <Route path="/mypage" element={<Mypage />}></Route>
+            {/* 임시로 질문상세페이지를 tags에서 볼 수 있도록 설정
             데이터 받아온 후에 다시 변경해야 함 */}
             <Route path="/tags" element={<Question />}></Route>
             <Route path="/users" element={<Pre />}></Route>
--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,35 +1,61 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const ButtonTemplate = styled.button`
-  width: ${(props) => props.width || "59.46px"};
-  height: ${(props) => props.height || "33px"};
+  width: ${(props) => props.width || '59.46px'};
+  height: ${(props) => props.height || '33px'};
 
   padding: 7px;
-  margin: ${(props) => props.margin || "0 0 0 4px"};
-  color: ${(props) => props.color || "#487299"};
-  background-color: ${(props) => props.bgColor || "#E3ECF3"};
+  margin: ${(props) => props.margin || '0 0 0 4px'};
+  color: ${(props) => props.color || '#487299'};
+  background-color: ${(props) => props.bgColor || '#E3ECF3'};
   align-items: center;
   text-align: center;
   font-size: 12px;
-  border-radius: ${(props) => props.bdradius || "4px"};
-  border: ${(props) => props.border || "1px solid #83A6C4"};
+  border-radius: ${(props) => props.bdradius || '4px'};
+  border: ${(props) => props.border || '1px solid #83A6C4'};
 
   outline: none;
   cursor: pointer;
-  box-shadow: ${(props) => props.boxshadow || "inset 0px 1px white"};
+  box-shadow: ${(props) => props.boxshadow || 'inset 0px 1px white'};
   &:hover {
-    background-color: ${(props) => props.hoverColor || "#b9d3e8"};
+    background-color: ${(props) => props.hoverColor || '#b9d3e8'};
   }
   &:active {
-    background-color: ${(props) => props.activeColor || "#9ac8ed"};
+    background-color: ${(props) => props.activeColor || '#9ac8ed'};
   }
 `;
 
 export function Button(props) {
-  const { onClick, type, text, width, margin, color, border, bgColor, hoverColor, activeColor, boxshadow, bdradius, height } = props;
+  const {
+    onClick,
+    type,
+    text,
+    width,
+    margin,
+    color,
+    border,
+    bgColor,
+    hoverColor,
+    activeColor,
+    boxshadow,
+    bdradius,
+    height,
+  } = props;
   return (
-    <ButtonTemplate onClick={onClick} type={type} margin={margin} color={color} bgColor={bgColor} border={border} hoverColor={hoverColor} activeColor={activeColor} width={width} boxshadow={boxshadow} bdradius={bdradius} height={height}>
+    <ButtonTemplate
+      onClick={onClick}
+      type={type}
+      margin={margin}
+      color={color}
+      bgColor={bgColor}
+      border={border}
+      hoverColor={hoverColor}
+      activeColor={activeColor}
+      width={width}
+      boxshadow={boxshadow}
+      bdradius={bdradius}
+      height={height}>
       {text}
     </ButtonTemplate>
   );
