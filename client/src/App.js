d975891537e8680e4ee200b3b54fb07f6093a742
Lee Hyun Ji <107021273+Hyunji0911@users.noreply.github.com>
2022-12-26 10:24:57 +0900

    Feature/fe/ui001 (#60)
    * feature: add router & modify css
    
    * style : AskQuestion modify css
    
    * feat: prettier & eslint settings
    
    Co-authored-by: wihyun <wihyun1123@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
-
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createGlobalStyle, css } from 'styled-components';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    useLocation,
+} from 'react-router-dom';
 import Template from './components/Template';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
@@ -15,13 +19,59 @@ import QuestionsList from './components/QuestionsList';
 import AskQuestion from './components/AskQuestion';
 import Mypage from './components/Mypage';
 import Pre from './components/Pre';
-import Question from './components/Question';
+
+function App() {
+    const location = useLocation();
+    const pathname = location.pathname;
+    const sidebar = ['/login', '/signup', '/askquestions'];
+    const ad = [...sidebar, '/mypage'];
+    const footer = ['/login', '/signup'];
+    return (
+        <>
+            <GlobalStyle pathname={pathname} />
+            <Navbar />
+            <Template>
+                {sidebar.indexOf(pathname) === -1 && <Sidebar />}
+                <Section>
+                    <Routes>
+                        <Route path="/" element={<TopQuestions />}></Route>
+                        <Route path="/login" element={<Login />}></Route>
+                        <Route path="/signup" element={<SignUp />}></Route>
+                        <Route
+                            path="/QuestiosList"
+                            element={<QuestionsList />}
+                        ></Route>
+                        <Route
+                            path="/askquestions"
+                            element={<AskQuestion />}
+                        ></Route>
+                        <Route path="/mypage" element={<Mypage />}></Route>
+                        <Route path="/tags" element={<Pre />}></Route>
+                        <Route path="/users" element={<Pre />}></Route>
+                        <Route path="/companies" element={<Pre />}></Route>
+                    </Routes>
+                </Section>
+                {ad.indexOf(pathname) === -1 && <Ad />}
+            </Template>
+            {footer.indexOf(pathname) === -1 && <Footer />}
+        </>
+    );
+}
 
 const GlobalStyle = createGlobalStyle`
 * {
   box-sizing: border-box;
 
 }
+
+body{
+      background-color:white;
+      ${(props) =>
+          ['/askquestions'].indexOf(props.pathname) !== -1 &&
+          css`
+              background-color: #f1f2f3;
+          `}
+      }
 
   ol, ul, li {
 	list-style: none;
@@ -30,37 +80,36 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 function App() {
-  const location = useLocation();
-  const pathname = location.pathname;
-  const sidebar = ["/login", "/signup"];
-  const ad = [...sidebar, "/mypage"];
-  const footer = [...sidebar];
-  return (
-    <>
-      <GlobalStyle />
-      <Navbar />
-      <Template>
-        {sidebar.indexOf(pathname) === -1 && <Sidebar />}
-        <Section>
-          <Routes>
-            <Route path="/" element={<TopQuestions />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/signup" element={<SignUp />}></Route>
-            <Route path="/QuestiosList" element={<QuestionsList />}></Route>
-            <Route path="/askquestions" element={<AskQuestion />}></Route>
-            <Route path="/mypage" element={<Mypage />}></Route>
-            {/* 임시로 질문상세페이지를 tags에서 볼 수 있도록 설정
-				데이터 받아온 후에 다시 변경해야 함 */}
-            <Route path="/tags" element={<Question />}></Route>
-            <Route path="/users" element={<Pre />}></Route>
-            <Route path="/companies" element={<Pre />}></Route>
-          </Routes>
-        </Section>
-        {ad.indexOf(pathname) === -1 && <Ad />}
-      </Template>
-      {footer.indexOf(pathname) === -1 && <Footer />}
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <GlobalStyle />
+            <Navbar />
+            <Template>
+                <Sidebar />
+                <Section>
+                    <Routes>
+                        <Route path="/" element={<TopQuestions />}></Route>
+                        <Route path="/login" element={<Login />}></Route>
+                        <Route path="/signup" element={<SignUp />}></Route>
+                        <Route
+                            path="/questions"
+                            element={<QuestionsList />}
+                        ></Route>
+                        <Route
+                            path="/askquestions"
+                            element={<AskQuestion />}
+                        ></Route>
+                        <Route path="/mypage" element={<Mypage />}></Route>
+                        <Route path="/tags" element={<Pre />}></Route>
+                        <Route path="/users" element={<Pre />}></Route>
+                        <Route path="/companies" element={<Pre />}></Route>
+                    </Routes>
+                </Section>
+                <Ad />
+            </Template>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
