934dc7282f3794f5161a3c018284caf8e5768ddc
wihyun <wihyun1123@gmail.com>
2022-12-28 14:55:44 +0900

    feature: askQuestion fetch API

--- client/src/util/usefetchQuestion.js
+++ client/src/util/usefetchQuestion.js
@@ -1,5 +1,5 @@
-export const fetchQuestionList = async () => {
-  return fetch("https://server.prestack41-25.kro.kr/api/v1/questions", {
+export const fetchQuestionList = async (id = 0) => {
+  return fetch(`https://server.prestack41-25.kro.kr/api/v1/questions${id !== 0 ? `/${id}` : ""}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json;charset=UTF-8",
@@ -9,6 +9,28 @@ export const fetchQuestionList = async () => {
     .then((res) => {
       if (!res.ok) {
         throw Error("nonono");
+      }
+      return res.json();
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
+};
+
+export const fetchCreateQuestion = async (data) => {
+  console.log(JSON.stringify(data));
+  return fetch("https://server.prestack41-25.kro.kr/api/v1/questions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+      Accept: "application/json",
+      Authorization: sessionStorage.getItem("access_token"),
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("잘못된 요청");
       }
       return res.json();
     })
