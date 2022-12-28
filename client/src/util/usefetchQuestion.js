b29a8be725aed259855ccad9cebd085fcda8fff6
RiOAOn <111356791+RiOAOn@users.noreply.github.com>
2022-12-28 15:45:19 +0900

    feat: answers 상태 관리 (#95)
    * feat: question data
    
    * feat: answer 상태관리

--- client/src/util/usefetchQuestion.js
+++ client/src/util/usefetchQuestion.js
@@ -1,14 +1,21 @@
 export const fetchQuestionList = async (questionId = 0) => {
-  return fetch(`https://server.prestack41-25.kro.kr/api/v1/questions${questionId !== 0 ? `/${questionId}` : ""}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json;charset=UTF-8",
-      Accept: "application/json",
-    },
-  })
+
+  return fetch(
+    `https://server.prestack41-25.kro.kr/api/v1/questions${
+      questionId !== 0 ? `/${questionId}` : ''
+    }`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        Accept: 'application/json',
+      },
+    }
+  )
+
     .then((res) => {
       if (!res.ok) {
-        throw Error("nonono");
+        throw Error('nonono');
       }
       return res.json();
     })
