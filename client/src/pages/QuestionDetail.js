95954c20ded4eccddcd8e86e5992884c492486ee
wihyun <wihyun1123@gmail.com>
2022-12-29 12:34:39 +0900

    feature: answer CRUD, question UD

--- client/src/pages/QuestionDetail.js
+++ client/src/pages/QuestionDetail.js
@@ -1,23 +1,26 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Button } from '../components/Button';
-import { GoTriangleUp, GoTriangleDown } from 'react-icons/go';
-import { BsBookmark } from 'react-icons/bs';
-import { GiBackwardTime } from 'react-icons/gi';
-import { MdAccountCircle } from 'react-icons/md';
-import { useEffect } from 'react';
-import { fetchQuestionList } from '../util/usefetchQuestion';
-import { questionDetailState, answersState } from '../state/atom';
-import { useRecoilState } from 'recoil';
-import { useLocation, Link } from 'react-router-dom';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Button } from "../components/Button";
+import { GoTriangleUp, GoTriangleDown } from "react-icons/go";
+import { BsBookmark } from "react-icons/bs";
+import { GiBackwardTime } from "react-icons/gi";
+import { MdAccountCircle } from "react-icons/md";
+import { useEffect } from "react";
+import { fetchQuestionList } from "../util/usefetchQuestion";
+import { questionDetailState, answersState } from "../state/atom";
+import { useRecoilState } from "recoil";
+import { useLocation, Link } from "react-router-dom";
+import { fetchCreateAnswer } from "../util/useFetchAnswer";
 
-import Loading from '../components/Loading';
+import Loading from "../components/Loading";
 
 export default function QuestionsDetail() {
   // 투표 수 voteResult로 바꾸고 상태관리
   const [voteCount, setVoteCount] = useState(0);
   // 로딩중 상태관리
   const [isLoading, setLoading] = useState(true);
+  const [content, setContent] = useState("");
+
   const [questionDetail, setquestionDetail] = useRecoilState(questionDetailState);
   const [answers, setAnswers] = useRecoilState(answersState);
 
@@ -39,12 +42,19 @@ export default function QuestionsDetail() {
     fetchQuestionList(pathname.slice(16))
       .then((data) => {
         setAnswers(data.data.answers);
-        console.log(data.data.answers);
       })
       .catch((err) => {
         console.log(err.message);
       });
   }, []);
+  function contentHandler(e) {
+    setContent(e.target.value);
+  }
+  const postAnswer = async () => {
+    await fetchCreateAnswer(questionDetail.questionId, content).then((data) => {
+      window.location.href = `${questionDetail.questionId}`;
+    });
+  };
 
   if (isLoading) {
     return <Loading />;
@@ -55,16 +65,7 @@ export default function QuestionsDetail() {
           <Title>
             <h1>{questionDetail.title}</h1>
             <Link to="/askquestions">
-              <Button
-                text="Ask Question"
-                color="white"
-                border="1px solid #4393F7"
-                bgColor="#4393F7"
-                hoverColor="#2D75C6"
-                activeColor="#1859A3"
-                width="103.02px"
-                boxshadow="inset 0px 1px hsl(206,90%,69.5%)"
-              />
+              <Button text="Ask Question" color="white" border="1px solid #4393F7" bgColor="#4393F7" hoverColor="#2D75C6" activeColor="#1859A3" width="103.02px" boxshadow="inset 0px 1px hsl(206,90%,69.5%)" />
             </Link>
           </Title>
           <Info>Asked today Modified today Viewed 13 times</Info>
@@ -99,9 +100,9 @@ export default function QuestionsDetail() {
           {answers.map((answer) => (
             <MainContainer>
               <VoteContainer>
-                <GoTriangleUp className="triangle" onClick={() => setVoteCount(voteCount + 1)} />
+                <GoTriangleUp className="triangle" onClick={setVoteCount(voteCount + 1)} />
                 <div>{voteCount}</div>
-                <GoTriangleDown className="triangle" onClick={() => setVoteCount(voteCount - 1)} />
+                <GoTriangleDown className="triangle" onClick={setVoteCount(voteCount - 1)} />
                 <BsBookmark className="icon" />
                 <GiBackwardTime className="icon" />
               </VoteContainer>
@@ -109,115 +110,104 @@ export default function QuestionsDetail() {
             </MainContainer>
           ))}
           Your Answer
-          <textarea />
-          <Button
-            text="Post Your Answer"
-            color="white"
-            border="1px solid #4393F7"
-            bgColor="#4393F7"
-            hoverColor="#2D75C6"
-            activeColor="#1859A3"
-            width="119.77px"
-            height="54.77px"
-            boxshadow="inset 0px 1px hsl(206,90%,69.5%)"
-          />
+          <textarea onChange={contentHandler} />
+          <Button onClick={postAnswer} text="Post Your Answer" color="white" border="1px solid #4393F7" bgColor="#4393F7" hoverColor="#2D75C6" activeColor="#1859A3" width="119.77px" height="54.77px" boxshadow="inset 0px 1px hsl(206,90%,69.5%)" />
         </AnswerContainer>
       </Contents>
     );
   }
-
 }
 
 const Contents = styled.div`
-	padding: 20px;
-	height: 100%;
+  padding: 20px;
+  height: 100%;
 `;
 const Head = styled.div``;
 const Title = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const Info = styled.div`
-	padding-bottom: 10px;
+  padding-bottom: 10px;
 `;
 
 const MainContainer = styled.div`
-	display: flex;
-	justify-content: row;
-	padding: 20px 0px;
+  display: flex;
+  justify-content: row;
+  padding: 20px 0px;
 `;
 
 const VoteContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-self: center;
-	align-items: flex-start;
-	color: #babfc4;
-	cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  align-items: flex-start;
+  color: #babfc4;
+  cursor: pointer;
 
-	> div {
-		font-size: 20px;
-		/* 추후 수정사항 : text 가운데 정렬 불가 (일단 padding 사용)*/
-		padding: 5px 13px;
-		color: #6a737c;
-	}
+  > div {
+    font-size: 20px;
+    /* 추후 수정사항 : text 가운데 정렬 불가 (일단 padding 사용)*/
+    padding: 5px 13px;
+    color: #6a737c;
+  }
 
-	.triangle {
-		font-size: 40px;
-	}
+  .triangle {
+    font-size: 40px;
+  }
 
-	> .icon {
-		font-size: 20px;
-		margin: 9px;
-	}
+  > .icon {
+    font-size: 20px;
+    margin: 9px;
+  }
 `;
 
 const Maintext = styled.article`
-	background-color: red;
-	padding-left: 10px;
-	width: 100%;
+  background-color: red;
+  padding-left: 10px;
+  width: 100%;
 `;
 const SubContainer = styled.div`
-	display: flex;
-	flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 const TagBox = styled.div`
-	display: flex;
-	padding-left: 40px;
+  display: flex;
+  padding-left: 40px;
 `;
 const Tag = styled.div`
-	background-color: #e1ecf4;
-	border-radius: 3px;
-	padding: 5px;
-	margin: 5px;
-	color: #39739d;
-	font-size: 12px;
+  background-color: #e1ecf4;
+  border-radius: 3px;
+  padding: 5px;
+  margin: 5px;
+  color: #39739d;
+  font-size: 12px;
 `;
 const UserInfo = styled.div`
-	display: flex;
-	flex-direction: column;
-	padding: 10px;
-	font-size: 12px;
-	background-color: #e1ecf4;
-	border-radius: 3px;
-	width: 200px;
-	height: 70px;
-	align-self: end;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  font-size: 12px;
+  background-color: #e1ecf4;
+  border-radius: 3px;
+  width: 200px;
+  height: 70px;
+  align-self: end;
 `;
 const Name = styled.div`
-	display: flex;
-	align-items: center;
-	font-size: 14px;
-	color: #0074cc;
-	font-weight: bold;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  color: #0074cc;
+  font-weight: bold;
 
-	> svg {
-		color: black;
-		margin: 5px 10px 0px 0px;
-		font-size: 35px;
-	}
+  > svg {
+    color: black;
+    margin: 5px 10px 0px 0px;
+    font-size: 35px;
+  }
 `;
 
 const AnswerContainer = styled.div`
