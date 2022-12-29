1a0bad025ae5c4213c7f4c2b911b43cb0d8d46fc
Lee Hyun Ji <107021273+Hyunji0911@users.noreply.github.com>
2022-12-29 13:23:06 +0900

    Style/fe/question detail (#121)
    * style: QuestionDetail modify css
    
    * style: QuestionDetail modify css
    
    * feature: App.js modify - default logout state

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -15,128 +15,191 @@ import { userInfoState } from "../state/atom";
 import { useRecoilState } from "recoil";
 
 export default function Login() {
-  const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const setIsLogin = useSetRecoilState(loginState);
-  const [userInfo, setuserInfo] = useRecoilState(userInfoState);
+	const navigate = useNavigate();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const setIsLogin = useSetRecoilState(loginState);
+	const [userInfo, setuserInfo] = useRecoilState(userInfoState);
 
-  function emailHandler(e) {
-    setEmail(e.target.value);
-  }
-  function passwordHandler(e) {
-    setPassword(e.target.value);
-  }
+	function emailHandler(e) {
+		setEmail(e.target.value);
+	}
+	function passwordHandler(e) {
+		setPassword(e.target.value);
+	}
 
-  const login = async () => {
-    const loginData = JSON.stringify({
-      email: email, //test1@gmail.com
-      password: password, //password
-    });
-    await fetchLogin(loginData).then((data) => {
-      if (data.status === 200) {
-        setIsLogin(true);
-        fetchMemberInfo()
-          .then((data) => {
-            setuserInfo(data.data);
-          })
-          .catch((err) => {
-            console.log(err.message);
-          });
-        navigate("/");
-      }
-    });
-  };
+	const login = async () => {
+		const loginData = JSON.stringify({
+			email: email, //test1@gmail.com
+			password: password, //password
+		});
+		await fetchLogin(loginData).then((data) => {
+			if (data.status === 200) {
+				setIsLogin(true);
+				fetchMemberInfo()
+					.then((data) => {
+						setuserInfo(data.data);
+					})
+					.catch((err) => {
+						console.log(err.message);
+					});
+				navigate("/");
+			}
+		});
+	};
 
-  return (
-    <Content>
-      <Logo src={icon} alt="logo" />
-      <Button text="Log In with Google" color="black" border="1px solid #bbbfc3" bgColor="white" hoverColor="hsl(210,8%,97.5%)" activeColor="hsl(210,8%,97.5%)" width="288.445px" margin="6px 0px" boxshadow="none" />
-      <Button text="Log In with GitHub" color="white" border="none" bgColor="black" hoverColor="none" activeColor="hsl(210,8%,97.5%)" width="288.445px" margin="6px 0px" boxshadow="none" />
-      <Button text="Log In with FaceBook" color="white" border="none" bgColor="#385499" hoverColor="#314a86" activeColor="#2a4074" width="288.445px" margin="6px 0px" boxshadow="none"></Button>
-      <FormContainer>
-        <LabelTextInput onChange={emailHandler} id="email" text="Email" placeholder="Please enter your e-mail" />
-        <LabelTextInput onChange={passwordHandler} id="password" text="Password" placeholder="Please enter your password" />
-        <Button onClick={login} type="button" text="Log In" color="white" border="1px solid #4393F7" bgColor="#4393F7" hoverColor="#2D75C6" activeColor="#1859A3" width="240.45px" margin="6px 0px" />
-      </FormContainer>
-      <SignUpText>
-        Don’t have an account?
-        <Link to="/signup">Sign Up</Link>
-      </SignUpText>
-    </Content>
-  );
+	return (
+		<Content>
+			<Logo
+				src={icon}
+				alt="logo"
+			/>
+			<Button
+				text="Log In with Google"
+				color="black"
+				border="1px solid #bbbfc3"
+				bgColor="white"
+				hoverColor="hsl(210,8%,97.5%)"
+				activeColor="hsl(210,8%,97.5%)"
+				width="288.445px"
+				margin="6px 0px"
+				boxshadow="none"
+			/>
+			<Button
+				text="Log In with GitHub"
+				color="white"
+				border="none"
+				bgColor="black"
+				hoverColor="none"
+				activeColor="hsl(210,8%,97.5%)"
+				width="288.445px"
+				margin="6px 0px"
+				boxshadow="none"
+			/>
+			<Button
+				text="Log In with FaceBook"
+				color="white"
+				border="none"
+				bgColor="#385499"
+				hoverColor="#314a86"
+				activeColor="#2a4074"
+				width="288.445px"
+				margin="6px 0px"
+				boxshadow="none"></Button>
+			<FormContainer>
+				<LabelTextInput
+					onChange={emailHandler}
+					id="email"
+					text="Email"
+					placeholder="Please enter your e-mail"
+				/>
+				<LabelTextInput
+					onChange={passwordHandler}
+					id="password"
+					text="Password"
+					placeholder="Please enter your password"
+				/>
+				<Button
+					onClick={login}
+					type="button"
+					text="Log In"
+					color="white"
+					border="1px solid #4393F7"
+					bgColor="#4393F7"
+					hoverColor="#2D75C6"
+					activeColor="#1859A3"
+					width="240.45px"
+					margin="6px 0px"
+				/>
+			</FormContainer>
+			<SignUpText>
+				Don’t have an account?
+				<Link to="/signup">Sign Up</Link>
+			</SignUpText>
+		</Content>
+	);
 }
 
 const Content = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: #f1f2f3;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+	width: 100%;
+	height: 100vh;
+	background-color: #f1f2f3;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 `;
 
 const FormContainer = styled.form`
-  width: 288.445px;
-  height: 234.2px;
+	width: 288.445px;
+	height: 234.2px;
 
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 
-  border: none;
-  background-color: #fff;
-  border-radius: 7px;
-  box-shadow: 0 10px 24px hsla(0, 0%, 0%, 0.05), 0 20px 48px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.1);
-  margin: 24px 0px 24px 0px;
+	border: none;
+	background-color: #fff;
+	border-radius: 7px;
+	box-shadow: 0 10px 24px hsla(0, 0%, 0%, 0.05),
+		0 20px 48px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.1);
+	margin: 24px 0px 24px 0px;
 `;
 
 const Logo = styled.img`
-  width: 30px;
-  margin-bottom: 24px;
+	width: 30px;
+	margin-bottom: 24px;
 `;
 
 const SignUpText = styled.span`
-  width: 288.45px;
-  height: 78px;
-  padding: 16px;
+	width: 288.45px;
+	height: 78px;
+	padding: 16px;
 
-  a {
-    text-decoration: none;
-    color: #2d75c6;
-    margin-left: 5px;
-  }
+	a {
+		text-decoration: none;
+		color: #2d75c6;
+		margin-left: 5px;
+	}
 `;
 
 const InputContainer = styled.div`
-  width: 240.45px;
-  margin: 6px 0;
+	width: 240.45px;
+	margin: 6px 0;
 `;
 
 const Input = styled.input`
-  width: 240.45px;
-  height: 32.59px;
+	width: 240.45px;
+	height: 32.59px;
 
-  padding: 7.8px 9.1px;
-  border: 1px solid #bbbfc3;
-  border-radius: 3px;
-  outline: none;
+	padding: 7.8px 9.1px;
+	border: 1px solid #bbbfc3;
+	border-radius: 3px;
+	outline: none;
 `;
 
 const Label = styled.label`
-  display: block;
-  margin: 6px 0;
-  font-size: 16px;
-  font-weight: bold;
+	display: block;
+	margin: 6px 0;
+	font-size: 16px;
+	font-weight: bold;
 `;
 
-const LabelTextInput = ({ id = "", text = "", placeholder = "", onChange = (e) => {} }) => {
-  return (
-    <InputContainer>
-      <Label htmlFor={id}>{text}</Label>
-      <Input name={id} placeholder={placeholder} onChange={onChange} />
-    </InputContainer>
-  );
+const LabelTextInput = ({
+	id = "",
+	text = "",
+	placeholder = "",
+	onChange = (e) => {},
+}) => {
+	return (
+		<InputContainer>
+			<Label htmlFor={id}>{text}</Label>
+			<Input
+				name={id}
+				placeholder={placeholder}
+				onChange={onChange}
+			/>
+		</InputContainer>
+	);
 };
--- client/src/pages/QuestionDetail.js
+++ client/src/pages/QuestionDetail.js
@@ -15,215 +15,304 @@ import { fetchCreateAnswer } from "../util/useFetchAnswer";
 import Loading from "../components/Loading";
 
 export default function QuestionsDetail() {
-  // 투표 수 voteResult로 바꾸고 상태관리
-  const [voteCount, setVoteCount] = useState(0);
-  // 로딩중 상태관리
-  const [isLoading, setLoading] = useState(true);
-  const [content, setContent] = useState("");
+	// 투표 수 voteResult로 바꾸고 상태관리
+	const [voteCount, setVoteCount] = useState(0);
+	// 로딩중 상태관리
+	const [isLoading, setLoading] = useState(true);
+	const [content, setContent] = useState("");
 
-  const [questionDetail, setquestionDetail] = useRecoilState(questionDetailState);
-  const [answers, setAnswers] = useRecoilState(answersState);
+	const [questionDetail, setquestionDetail] =
+		useRecoilState(questionDetailState);
+	const [answers, setAnswers] = useRecoilState(answersState);
 
-  const location = useLocation();
-  const pathname = location.pathname;
+	const location = useLocation();
+	const pathname = location.pathname;
 
-  useEffect(() => {
-    fetchQuestionList(pathname.slice(16))
-      .then((data) => {
-        setquestionDetail(data.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
+	useEffect(() => {
+		fetchQuestionList(pathname.slice(16))
+			.then((data) => {
+				setquestionDetail(data.data);
+				setLoading(false);
+			})
+			.catch((err) => {
+				console.log(err.message);
+			});
+	}, []);
 
-  useEffect(() => {
-    fetchQuestionList(pathname.slice(16))
-      .then((data) => {
-        setAnswers(data.data.answers);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
-  function contentHandler(e) {
-    setContent(e.target.value);
-  }
-  const postAnswer = async () => {
-    await fetchCreateAnswer(questionDetail.questionId, content).then((data) => {
-      window.location.href = `${questionDetail.questionId}`;
-    });
-  };
+	useEffect(() => {
+		fetchQuestionList(pathname.slice(16))
+			.then((data) => {
+				setAnswers(data.data.answers);
+			})
+			.catch((err) => {
+				console.log(err.message);
+			});
+	}, []);
+	function contentHandler(e) {
+		setContent(e.target.value);
+	}
+	const postAnswer = async () => {
+		await fetchCreateAnswer(questionDetail.questionId, content).then((data) => {
+			window.location.href = `${questionDetail.questionId}`;
+		});
+	};
 
-  if (isLoading) {
-    return <Loading />;
-  } else {
-    return (
-      <Contents>
-        <Head>
-          <Title>
-            <h1>{questionDetail.title}</h1>
-            <Link to="/askquestions">
-              <Button text="Ask Question" color="white" border="1px solid #4393F7" bgColor="#4393F7" hoverColor="#2D75C6" activeColor="#1859A3" width="103.02px" boxshadow="inset 0px 1px hsl(206,90%,69.5%)" />
-            </Link>
-          </Title>
-          <Info>Asked today Modified today Viewed 13 times</Info>
-          <hr />
-        </Head>
-        <MainContainer>
-          <VoteContainer>
-            <GoTriangleUp className="triangle" onClick={() => setVoteCount(voteCount + 1)} />
-            <div>{voteCount}</div>
-            <GoTriangleDown className="triangle" onClick={() => setVoteCount(voteCount - 1)} />
-            <BsBookmark className="icon" />
-            <GiBackwardTime className="icon" />
-          </VoteContainer>
-          <Maintext>{questionDetail.content}</Maintext>
-        </MainContainer>
-        <SubContainer>
-          <TagBox>
-            {questionDetail.tag.map((tag) => {
-              return <Tag key={questionDetail.questionId}>{tag}</Tag>;
-            })}
-          </TagBox>
-          <UserInfo>
-            asked 59 mins ago
-            <Name>
-              <MdAccountCircle />
-              {questionDetail.displayName}
-            </Name>
-          </UserInfo>
-        </SubContainer>
-        <AnswerContainer>
-          <div>{answers.length} Answers</div>
-          {answers.map((answer) => (
-            <MainContainer>
-              <VoteContainer>
-                <GoTriangleUp className="triangle" onClick={() => setVoteCount(voteCount - 1)} />
-                <div>{voteCount}</div>
-                <GoTriangleDown className="triangle" onClick={() => setVoteCount(voteCount - 1)} />
-                <BsBookmark className="icon" />
-                <GiBackwardTime className="icon" />
-              </VoteContainer>
-              <Maintext>{answer.content}</Maintext>
-            </MainContainer>
-          ))}
-          Your Answer
-          <textarea onChange={contentHandler} />
-          <Button onClick={postAnswer} text="Post Your Answer" color="white" border="1px solid #4393F7" bgColor="#4393F7" hoverColor="#2D75C6" activeColor="#1859A3" width="119.77px" height="54.77px" boxshadow="inset 0px 1px hsl(206,90%,69.5%)" />
-        </AnswerContainer>
-      </Contents>
-    );
-  }
+	if (isLoading) {
+		return <Loading />;
+	} else {
+		return (
+			<Contents>
+				<Head>
+					<Title>
+						<h1>{questionDetail.title}</h1>
+						<Link to="/askquestions">
+							<Button
+								text="Ask Question"
+								color="white"
+								border="1px solid #4393F7"
+								bgColor="#4393F7"
+								hoverColor="#2D75C6"
+								activeColor="#1859A3"
+								width="103.02px"
+								boxshadow="inset 0px 1px hsl(206,90%,69.5%)"
+							/>
+						</Link>
+					</Title>
+					<Info>Asked today Modified today Viewed 13 times</Info>
+					<hr />
+				</Head>
+				<MainContainer>
+					<VoteContainer>
+						<GoTriangleUp
+							className="triangle"
+							onClick={() => setVoteCount(voteCount + 1)}
+						/>
+						<div>{voteCount}</div>
+						<GoTriangleDown
+							className="triangle"
+							onClick={() => setVoteCount(voteCount - 1)}
+						/>
+						<BsBookmark className="icon" />
+						<GiBackwardTime className="icon" />
+					</VoteContainer>
+					<Maintext>{questionDetail.content}</Maintext>
+				</MainContainer>
+				<SubContainer>
+					<TagBox>
+						{questionDetail.tag.map((tag) => {
+							return <Tag key={questionDetail.questionId}>{tag}</Tag>;
+						})}
+					</TagBox>
+					<UserInfo>
+						asked 59 mins ago
+						<Name>
+							<MdAccountCircle />
+							{questionDetail.displayName}
+						</Name>
+					</UserInfo>
+				</SubContainer>
+				<AnswerContainer>
+					<div>{answers.length} Answers</div>
+					{answers.map((answer) => (
+						<MainContainer>
+							<VoteContainer>
+								<GoTriangleUp
+									className="triangle"
+									onClick={() => setVoteCount(voteCount - 1)}
+								/>
+								<div>{voteCount}</div>
+								<GoTriangleDown
+									className="triangle"
+									onClick={() => setVoteCount(voteCount - 1)}
+								/>
+								<BsBookmark className="icon" />
+								<GiBackwardTime className="icon" />
+							</VoteContainer>
+							<Maintext>{answer.content}</Maintext>
+						</MainContainer>
+					))}
+					Your Answer
+					<textarea onChange={contentHandler} />
+					<Button
+						onClick={postAnswer}
+						text="Post Your Answer"
+						color="white"
+						border="1px solid #4393F7"
+						bgColor="#4393F7"
+						hoverColor="#2D75C6"
+						activeColor="#1859A3"
+						width="119.77px"
+						height="54.77px"
+						boxshadow="inset 0px 1px hsl(206,90%,69.5%)"
+					/>
+				</AnswerContainer>
+			</Contents>
+		);
+	}
 }
 
 const Contents = styled.div`
-  padding: 20px;
-  height: 100%;
+	padding: 20px;
+	height: 100%;
 `;
 const Head = styled.div``;
 const Title = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	h1 {
+		color: #3c4044;
+		font-size: 27px;
+		font-weight: 400;
+	}
 `;
 
 const Info = styled.div`
-  padding-bottom: 10px;
+	padding-bottom: 10px;
+	margin-bottom: 16px;
+	font-size: 13px;
+	border-bottom: 1px solid #e4e6e8;
+
+	div {
+		display: flex;
+	}
+
+	p {
+		margin: 0 16px 8px 0;
+	}
+
+	span {
+		color: #6c737b;
+	}
 `;
 
 const MainContainer = styled.div`
-  display: flex;
-  justify-content: row;
-  padding: 20px 0px;
+	display: flex;
+	justify-content: row;
+	padding: 20px 0px;
+	border-bottom: 1px solid #e4e6e8;
+	padding: 0 0 20px 0;
 `;
 
 const VoteContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-self: center;
-  align-items: flex-start;
-  color: #babfc4;
-  cursor: pointer;
+	margin-right: 16px;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	color: #babfc4;
+	cursor: pointer;
 
-  > div {
-    font-size: 20px;
-    /* 추후 수정사항 : text 가운데 정렬 불가 (일단 padding 사용)*/
-    padding: 5px 13px;
-    color: #6a737c;
-  }
+	> div {
+		font-size: 20px;
+		/* 추후 수정사항 : text 가운데 정렬 불가 (일단 padding 사용)*/
+		padding: 5px 13px;
+		color: #6a737c;
+	}
 
-  .triangle {
-    font-size: 40px;
-  }
+	.triangle {
+		font-size: 40px;
+	}
 
-  > .icon {
-    font-size: 20px;
-    margin: 9px;
-  }
+	> .icon {
+		font-size: 20px;
+		margin: 9px;
+	}
+`;
+
+const QuestionContainer = styled.div`
+	display: flex;
+	border-bottom: 1px solid #e4e6e8;
+	padding: 0 0 20px 0;
+`;
+
+const MainTextContainer = styled.div`
+	width: 100%;
 `;
 
 const Maintext = styled.article`
-  background-color: red;
-  padding-left: 10px;
-  width: 100%;
+	width: 100%;
+	margin-bottom: 16px;
+	font-size: 15px;
 `;
 const SubContainer = styled.div`
-  display: flex;
-  flex-direction: column;
+	display: flex;
+	flex-direction: column;
 `;
 const TagBox = styled.div`
-  display: flex;
-  padding-left: 40px;
+	display: flex;
+	margin-bottom: 13px;
 `;
+
 const Tag = styled.div`
-  background-color: #e1ecf4;
-  border-radius: 3px;
-  padding: 5px;
-  margin: 5px;
-  color: #39739d;
-  font-size: 12px;
+	background-color: #e1ecf4;
+	border-radius: 3px;
+	padding: 5px;
+	margin: 0 7px 0 0;
+	color: #39739d;
+	font-size: 12px;
 `;
+
+const CRUDandUserContainer = styled.div`
+	margin: 16px 0;
+	display: flex;
+	justify-content: space-between;
+`;
+
+const CRUDText = styled.div`
+	color: #6c737b;
+	font-size: 13px;
+
+	span {
+		margin: 0 7px 0 0;
+	}
+`;
+
 const UserInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 10px;
-  font-size: 12px;
-  background-color: #e1ecf4;
-  border-radius: 3px;
-  width: 200px;
-  height: 70px;
-  align-self: end;
+	display: flex;
+	flex-direction: column;
+	padding: 10px;
+	font-size: 12px;
+	background-color: #e1ecf4;
+	border-radius: 3px;
+	width: 200px;
+	height: 70px;
+	align-self: end;
 `;
 const Name = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 14px;
-  color: #0074cc;
-  font-weight: bold;
+	display: flex;
+	align-items: center;
+	font-size: 14px;
+	color: #0074cc;
+	font-weight: bold;
 
-  > svg {
-    color: black;
-    margin: 5px 10px 0px 0px;
-    font-size: 35px;
-  }
+	> svg {
+		color: black;
+		margin: 5px 10px 0px 0px;
+		font-size: 35px;
+	}
 `;
 
 const AnswerContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  font-size: 20px;
+	display: flex;
+	flex-direction: column;
+	font-size: 20px;
+	margin-top: 10px;
 
-  div {
-    padding: 10px;
-  }
-  textarea {
-    margin: 20px 5px;
-    height: 200px;
-  }
-`;
+	h2 {
+		margin: 19px 0 19px 0;
+		font-size: 19px;
+		font-weight: 400;
+	}
 
-const AnsCount = styled.div`
-  padding: 10px;
+	textarea {
+		height: 200px;
+		border-color: #bbbfc3;
+		border-radius: 5px;
+	}
+
+	div {
+		margin: 8px 0;
+		font-size: 19px;
+	}
 `;
