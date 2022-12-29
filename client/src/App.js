5087351d236b35f977ac8a1017d7f4a14df1a34e
wihyun <wihyun1123@gmail.com>
2022-12-29 15:19:52 +0900

    feature: editQues

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import AskQuestion from "./pages/AskQuestion";
 import Mypage from "./pages/Mypage";
 import Pre from "./pages/Pre";
 import QuestionDetail from "./pages/QuestionDetail";
+import EditQuestion from "./pages/EditQuestion";
 import { loginState } from "./state/atom";
 import { useRecoilState } from "recoil";
 import { fetchMemberInfo } from "./util/fetchLogin";
@@ -32,80 +33,62 @@ const GlobalStyle = createGlobalStyle`
    body {
    background-color: white;
    ${(props) =>
-			props.pathname === "/askquestions"
-				? css`
-						background-color: #f7f8f8;
-				  `
-				: ["/login", "/signup"].indexOf(props.pathname) !== -1 &&
-				  css`
-						background-color: #f1f2f3;
-				  `}
+     props.pathname === "/askquestions" || props.pathname === `/editQuestion/${props.editPathName}`
+       ? css`
+           background-color: #f7f8f8;
+         `
+       : ["/login", "/signup"].indexOf(props.pathname) !== -1 &&
+         css`
+           background-color: #f1f2f3;
+         `}
    }
 `;
 
 function App() {
-	const location = useLocation();
-	const pathname = location.pathname;
-	const sidebar = ["/login", "/signup", "/askquestions"];
-	const ad = [...sidebar, "/mypage", "/users", "/companies", "/tags"];
-	const footer = ["/login", "/signup"];
+  const location = useLocation();
+  const pathname = location.pathname;
+  const editPathName = pathname.slice(14);
+  const sidebar = ["/login", "/signup", "/askquestions", `/editQuestion/${editPathName}`];
+  const ad = [...sidebar, "/mypage", "/users", "/companies", "/tags"];
+  const footer = ["/login", "/signup"];
 
-	const [isLogin, setIsLogin] = useRecoilState(loginState);
+  const [isLogin, setIsLogin] = useRecoilState(loginState);
 
-	useEffect(() => {
-		fetchMemberInfo().then((data) => {
-			if (!data) {
-				sessionStorage.removeItem("access_token");
-				setIsLogin(false);
-				//   window.location.href = "/";
-			}
-		});
-	});
-	return (
-		<>
-			<GlobalStyle pathname={pathname} />
-			<Navbar />
-			<Template>
-				{sidebar.indexOf(pathname) === -1 && <Sidebar />}
-				<Section>
-					<Routes>
-						<Route
-							path="/"
-							element={isLogin ? <TopQuestions /> : <QuestionsList />}></Route>
-						<Route
-							path="/login"
-							element={<Login />}></Route>
-						<Route
-							path="/signup"
-							element={<SignUp />}></Route>
-						<Route
-							path="/questionslist"
-							element={<QuestionsList />}></Route>
-						<Route
-							path="/askquestions"
-							element={<AskQuestion />}></Route>
-						<Route
-							path="/mypage"
-							element={<Mypage />}></Route>
-						<Route
-							path="/questionDetail/:questionId"
-							element={<QuestionDetail />}></Route>
-						<Route
-							path="/tags"
-							element={<Pre />}></Route>
-						<Route
-							path="/users"
-							element={<Pre />}></Route>
-						<Route
-							path="/companies"
-							element={<Pre />}></Route>
-					</Routes>
-				</Section>
-				{ad.indexOf(pathname) === -1 && <Ad />}
-			</Template>
-			{footer.indexOf(pathname) === -1 && <Footer />}
-		</>
-	);
+  useEffect(() => {
+    fetchMemberInfo().then((data) => {
+      if (!data) {
+        sessionStorage.removeItem("access_token");
+        setIsLogin(false);
+        //   window.location.href = "/";
+      }
+    });
+  });
+  return (
+    <>
+      <GlobalStyle pathname={pathname} editPathName={editPathName} />
+      <Navbar />
+      <Template>
+        {sidebar.indexOf(pathname) === -1 && <Sidebar />}
+        <Section>
+          <Routes>
+            <Route path="/" element={isLogin ? <TopQuestions /> : <QuestionsList />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+            <Route path="/questionslist" element={<QuestionsList />}></Route>
+            <Route path="/askquestions" element={<AskQuestion />}></Route>
+            <Route path="/mypage" element={<Mypage />}></Route>
+            <Route path="/questionDetail/:questionId" element={<QuestionDetail />}></Route>
+            <Route path="/editQuestion/:questionId" element={<EditQuestion />}></Route>
+            <Route path="/tags" element={<Pre />}></Route>
+            <Route path="/users" element={<Pre />}></Route>
+            <Route path="/companies" element={<Pre />}></Route>
+          </Routes>
+        </Section>
+        {ad.indexOf(pathname) === -1 && <Ad />}
+      </Template>
+      {footer.indexOf(pathname) === -1 && <Footer />}
+    </>
+  );
 }
 
 export default App;
